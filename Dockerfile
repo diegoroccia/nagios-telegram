426f8278bbc31f6fad3e671fcdbefee1d9d76282
Diego Roccia <diego.roccia@gmail.com>
2017-07-19 13:56:51 +0200

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN pip3 install python-telegram-bot
 RUN pip3 install requests
 RUN pip3 install ipython
 
-ADD bin/bot.py /
+ADD bot.py /
+ADD local_settings.py /
 
 CMD python3 /bot.py 
--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
-#!python3
 from telegram.ext import Updater,CommandHandler
 import requests
 import logging
 import os
 import re
 
-configuration = { k.replace("BOT_",""): os.environ[k] for k in os.environ if  k.find("BOT_") == 0}
+config = { k.replace("BOT_",""): os.environ[k] for k in os.environ if  k.find("BOT_") == 0}
 
-print (configuration)
+from local_settings import *
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-updater = Updater("440051803:AAFwKlpWQntxw0V96EtXsUU7czdwIuhyrsE")
+updater = Updater(config["BOTKEY"])
 dispatcher = updater.dispatcher
 
 def problemlist(bot, update):                                               
-     print("received")
+     print("received", update.message )
      bot.send_message(chat_id=update.message.chat_id, text="let me check...")
-     r = requests.get("https://{NAGIOSUSER}:{NAGIOSPWD}@{NAGIOSURL}/nagios/cgi-bin/statusjson.cgi?query=servicelist&servicestatus=critical".format(**configuration))
+     r = requests.get("https://{NAGIOSUSER}:{NAGIOSPWD}@{NAGIOSURL}/nagios/cgi-bin/statusjson.cgi?query=servicelist&servicestatus=critical".format(**config))
      for host in r.json()['data']['servicelist']:
          for service in r.json()['data']['servicelist'][host]:
              bot.send_message(chat_id=update.message.chat_id, text="{} > {}".format(host,service))
